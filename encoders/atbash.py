82ee4e59658b1ab494ebad1881798b00f2133854
Gumn4m1 <98003893+Pankaj0038@users.noreply.github.com>
2024-01-15 23:54:57 +0530

    Update atbash.py

--- encoders/atbash.py
+++ encoders/atbash.py
@@ -1,13 +1,25 @@
+#!/usr/bin/python
+import string
+alp = string.ascii_lowercase()
+ALP = string.ascii_uppercase()
 def decatbash(message):
     cipher = ''
-    for letter in message.upper():
-        ascii = ord(letter)
-        if 65 <= ascii <= 90:
-            position = ascii - 65
-            newpos = 25 - position
-            newAscii = newpos + 65
-            newLetter = chr(newAscii)
+    for letter in message:
+        if letter in ALP:
+            ascii = ord(letter)
+            if 65 <= ascii <= 90:
+                position = ascii - 65
+                newpos = 25 - position
+                newAscii = newpos + 65
+                newLetter = chr(newAscii)
+        elif letter in alp:
+            ascii = ord(letter)
+            if 97 <= ascii <= 122:
+                position = ascii - 97
+                newpos = 25 - position
+                newAscii = newpos + 97
+                newLetter = chr(newAscii)
         else:
             newLetter = letter
-        cipher = cipher + newLetter
+        cipher += newLetter
     return cipher
