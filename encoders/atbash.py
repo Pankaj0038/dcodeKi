dde6dcf16a0ef26a9b6bafa23837321789cd99f8
daksh1210jain <daksh1210jain@gmail.com>
2024-01-15 23:00:42 +0530

    Made the necessay changes

--- encoders/atbash.py
+++ encoders/atbash.py
@@ -1,21 +1,13 @@
-lookup_table = {
-    'A': 'Z', 'B': 'Y', 'C': 'X', 'D': 'W', 'E': 'V',
-    'F': 'U', 'G': 'T', 'H': 'S', 'I': 'R', 'J': 'Q',
-    'K': 'P', 'L': 'O', 'M': 'N', 'N': 'M', 'O': 'L',
-    'P': 'K', 'Q': 'J', 'R': 'I', 'S': 'H', 'T': 'G',
-    'U': 'F', 'V': 'E', 'W': 'D', 'X': 'C', 'Y': 'B', 'Z': 'A',
-    'a': 'z', 'b': 'y', 'c': 'x', 'd': 'w', 'e': 'v',
-    'f': 'u', 'g': 't', 'h': 's', 'i': 'r', 'j': 'q',
-    'k': 'p', 'l': 'o', 'm': 'n', 'n': 'm', 'o': 'l',
-    'p': 'k', 'q': 'j', 'r': 'i', 's': 'h', 't': 'g',
-    'u': 'f', 'v': 'e', 'w': 'd', 'x': 'c', 'y': 'b', 'z': 'a',
-    ' ': ' '
-}
 def decatbash(message):
-	cipher = ''
-	for letter in (message):
-		if(letter != ' '):
-			cipher += lookup_table[letter]
-		else:
-			cipher += ' '
-	return cipher
+    cipher = ''
+    for letter in message.upper():
+        ascii = ord(letter)
+        if 65 <= ascii <= 90:
+            position = ascii - 65
+            newpos = 25 - position
+            newAscii = newpos + 65
+            newLetter = chr(newAscii)
+        else:
+            newLetter = letter
+        cipher = cipher + newLetter
+    return cipher
