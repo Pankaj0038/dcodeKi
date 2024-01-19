ae4b3e84b6ebda7ff042b8d5e7678b996bd88185
Gumn4m1 <98003893+Pankaj0038@users.noreply.github.com>
2024-01-19 23:46:29 +0530

    Update base58.py

--- encoders/base58.py
+++ encoders/base58.py
@@ -6,19 +6,15 @@ charset = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 def base58_decode(ciphertext):
     result = 0
     for char in ciphertext:
-        print(char)
         if char not in charset:
             raise ValueError(f"Invalid character '{char}' in Base58 input")
-        print("character ",result)
         result = result * 58 + charset.index(char)
-        print("result * 58 + index = ",result)
 
     result_bytes = bytearray()
     while result > 0:
         result_bytes.append(result % 256)
         print("bytes ",result_bytes)
         result //= 256
-        print("result // 256 =  ",result)
 
     decoded_text = result_bytes[::-1].decode('utf-8')
     return decoded_text
