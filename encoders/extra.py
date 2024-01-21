49cfeab9cc9743b3930188876dd3404cfd71ef4f
daksh1210jain <daksh1210jain@gmail.com>
2024-01-21 13:06:52 +0530

    added the changes

---
+++ encoders/extra.py
@@ -0,0 +1,14 @@
+def Upper(text):
+    z=str(text)
+    ciphertext=z.upper()
+    return ciphertext
+
+def Lower(text):
+    d=str(text)
+    ciphertext=d.lower()
+    return ciphertext
+
+def Reverse(text):
+    s=str(text)
+    ciphertext= s[::-1]
+    return ciphertext
