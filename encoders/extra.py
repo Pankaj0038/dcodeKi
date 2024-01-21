db577621a6b1a17c8b7f08716d3abbc1e6cf7d30
Gumn4m1 <98003893+Pankaj0038@users.noreply.github.com>
2024-01-21 15:57:39 +0530

    Update extra.py

--- encoders/extra.py
+++ encoders/extra.py
@@ -12,3 +12,16 @@ def Reverse(text):
     s=str(text)
     ciphertext= s[::-1]
     return ciphertext
+
+if __name__=="__main__":
+    text= input("Enter the text to operate: ")
+    choice = input("Enter your choice (u for uppercase / l for lowercase / r for reverse): ")
+    if choice=='u':
+        print(Upper(text))
+    elif choice=='l':
+        print(Lower(text))
+    elif choice =='r':
+        print(Reverse(text))
+    else:
+        print("Invalid choice!")
+        
