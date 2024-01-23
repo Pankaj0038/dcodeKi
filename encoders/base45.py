d84f8bd9d3257713828ca1ae834970dd4125f0ac
AnamNaaz-7 <115145295+AnamNaaz-7@users.noreply.github.com>
2024-01-23 16:15:17 +0530

    Create base45.py
    Code for base 45 decoder and encoder

---
+++ encoders/base45.py
@@ -0,0 +1,41 @@
+class Base45:
+    BASE45_CHARSET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:"
+
+    @staticmethod
+    def encode(data):
+        result = []
+        data_len = len(data)
+        for i in range(0, data_len, 2):
+            if i + 1 < data_len:
+                x = (data[i] << 8) + data[i+1]
+                result.append(Base45.BASE45_CHARSET[x % 45])
+                result.append(Base45.BASE45_CHARSET[(x // 45) % 45])
+                result.append(Base45.BASE45_CHARSET[x // (45*45)])
+            else:
+                x = data[i]
+                result.append(Base45.BASE45_CHARSET[x % 45])
+                result.append(Base45.BASE45_CHARSET[x // 45])
+
+        return ''.join(result).rstrip('\0')
+
+    @staticmethod
+    def decode(data):
+        result = bytearray()
+        data_len = len(data)
+        for i in range(0, data_len, 3):
+            x = Base45.BASE45_CHARSET.index(data[i]) + Base45.BASE45_CHARSET.index(data[i+1]) * 45
+            if i + 2 < data_len:
+                x += Base45.BASE45_CHARSET.index(data[i+2]) * 45 * 45
+                result.append(x >> 8)
+            result.append(x & 0xFF)
+
+        return bytes(result)
+
+# Example usage
+original_data = "Hello, World!"
+encoded_data = Base45.encode(original_data.encode())
+decoded_data = Base45.decode(encoded_data)
+
+print("Original Data:", original_data)
+print("Encoded Data:", encoded_data)
+print("Decoded Data:", decoded_data.decode())
