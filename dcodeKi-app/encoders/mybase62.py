1cc41020d8549d39fd2e098b1dd9d3e6a0390410
Moxie <ysmarina1234@gmail.com>
2024-01-27 01:25:35 +0530

    updating the base62 decode code

--- dcodeKi-app/encoders/mybase62.py
+++ dcodeKi-app/encoders/mybase62.py
@@ -21,6 +21,21 @@ def encode(input_str):
     return ''.join(encoded)
 
 
+def decode(encoded):
+    decoded_bytes = bytearray()
+    for i in range(0, len(encoded), 3):
+        chunk = encoded[i:min(i + 3, len(encoded))]
+        val = 0
+        for char in chunk:
+            val = val * 62 + charset.index(char)
+        chunk_hex = format(val, 'x')
+        if(binascii.unhexlify(chunk_hex.encode()) == bytes()):
+            return f"The given input '{encoded}' is not a valid base62 string"
+        dst = binascii.unhexlify(chunk_hex.encode())
+        decoded_bytes.extend(dst)
+    return decoded_bytes.decode()
+
+
 # if __name__ == "__main__":
 #     usr_input = input("Enter the string to be encoded/decoded: ")
 #     op = input("Enter 'e' for encode and 'd' for decode: ")
