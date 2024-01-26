eb4b5dab20a4997dbc3ba3d63cb74690af36953f
Moxie <ysmarina1234@gmail.com>
2024-01-27 01:40:51 +0530

    updating base62 decode fxn

--- dcodeKi-app/encoders/mybase62.py
+++ dcodeKi-app/encoders/mybase62.py
@@ -29,19 +29,20 @@ def decode(encoded):
         for char in chunk:
             val = val * 62 + charset.index(char)
         chunk_hex = format(val, 'x')
-        if(binascii.unhexlify(chunk_hex.encode()) == bytes()):
-            return f"The given input '{encoded}' is not a valid base62 string"
-        dst = binascii.unhexlify(chunk_hex.encode())
+        try:
+            dst = binascii.unhexlify(chunk_hex.encode())
+        except binascii.Error:
+            return f"Invalid base62 encoded string: {encoded}"
         decoded_bytes.extend(dst)
     return decoded_bytes.decode()
 
 
-# if __name__ == "__main__":
-#     usr_input = input("Enter the string to be encoded/decoded: ")
-#     op = input("Enter 'e' for encode and 'd' for decode: ")
-#     if op == "e":
-#         print(encode(usr_input), decode(encode(usr_input)))
-#     elif op == "d":
-#         print(decode(usr_input))
-#     else:
-#         print("Invalid option")
+if __name__ == "__main__":
+    usr_input = input("Enter the string to be encoded/decoded: ")
+    op = input("Enter 'e' for encode and 'd' for decode: ")
+    if op == "e":
+        print(encode(usr_input), decode(encode(usr_input)))
+    elif op == "d":
+        print(decode(usr_input))
+    else:
+        print("Invalid option")
