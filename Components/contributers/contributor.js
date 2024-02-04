9a9037543152bca7411b17d066ae14d227ac49dc
Moxie <ysmarina1234@gmail.com>
2024-02-04 13:06:00 +0530

    updating fxn to fetch from API

--- Components/contributers/contributor.js
+++ Components/contributers/contributor.js
@@ -5,20 +5,32 @@ let allContributorsGithub = [];
 let allContributorsAvatar = [];
 
 function fetchAllContributors() {
-    allContributorsGithub.forEach((githubLink, index) => {
-        const contributorCard = document.createElement('div');
-        contributorCard.classList.add('contributor-card');
 
-        const avatar = document.createElement('img');
-        avatar.src = allContributorsAvatar[index];
+    fetch('https://api.github.com/repos/Pankaj0038/dcodeKi/contributors')
+        .then(response => response.json())
+        .then(data => {
 
-        const link = document.createElement('a');
-        link.target = '_blank';
-        link.href = githubLink;
-        link.appendChild(avatar);
+            data.forEach(contributor => {
+                let modified_avatar_url = contributor.avatar_url.replace('v=4', 's=64&v=4');
+                allContributorsGithub.push(contributor.html_url);
+                allContributorsAvatar.push(modified_avatar_url);
+            });
 
-        contributorCard.appendChild(link);
-        contributor.appendChild(contributorCard);
-    });
+            allContributorsGithub.forEach((githubLink, index) => {
+                const contributorCard = document.createElement('div');
+                contributorCard.classList.add('contributor-card');
+
+                const avatar = document.createElement('img');
+                avatar.src = allContributorsAvatar[index];
+
+                const link = document.createElement('a');
+                link.target = '_blank';
+                link.href = githubLink;
+                link.appendChild(avatar);
+
+                contributorCard.appendChild(link);
+                contributor.appendChild(contributorCard);
+            });
+        })
 }
 fetchAllContributors();
